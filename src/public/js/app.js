47c4f57a81bd9fd64db63f5bc3758c65072e98bb
sskskfskfj <dongkyun7717@gmail.com>
2024-08-28 20:54:22 +0900

    aaa

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,26 @@
 //webSocket -> 서버로의 연결
 const webSocket = new WebSocket(`ws://${window.location.host}`)
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("form")
 
 webSocket.addEventListener("open", ()=>{
     console.log("connected to server")
 })
 
 webSocket.addEventListener("message", (message)=>{
-    console.log("server message", message.timeStamp)
+    console.log("new message", message.data)
 })
 
 webSocket.addEventListener("close", ()=>{
     console.log("disconnected to server")
 })
+
+function handleSubmit(event){
+    event.preventDefault()
+    const input = messageForm.querySelector("input")
+    console.log(input.value)
+    webSocket.send(input.value)
+    input.value = ""
+}
+
+messageForm.addEventListener("submit", handleSubmit)
--- src/server.js
+++ src/server.js
@@ -14,11 +14,22 @@ const handleListen = ()=>console.log(`listening on https://localhost:3000`)
 const server = http.createServer(app)
 const wss = new WebSocket.Server({server})
 
+
+function socketDisconnected(){console.log("client 연결 끊김")}
+
+//가짜 데이터베이스
+const webSockets = []
+
 //webSocket은 브라우저와의 연결
 wss.on("connection",(webSocket)=>{
-    webSocket.send("hello")
+    webSockets.push(webSocket)
+    webSocket.on("close", socketDisconnected)
     console.log("connected to browser")
-})
+    webSocket.on("message", (message)=>{
+        webSockets.forEach(aSocket => aSocket.send(message.toString("utf-8")))
+        console.log(message.toString("utf-8"))
+    })
+}) 
 
 server.listen(3000, handleListen)
 
