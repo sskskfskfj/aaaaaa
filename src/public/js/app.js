2abeab2c36fa88a4befe066e55363a6e129dda04
sskskfskfj <dongkyun7717@gmail.com>
2024-09-11 23:29:57 +0900

    오류 수정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,40 +4,40 @@ const form = document.querySelector('form')
 const room =  document.getElementById('room')
 
 room.hidden = true;
-let roomName
+let roomName = "";
 
 function addMessage(message){
-    const ul = document.querySelector("ul")
-    const li = ul.createElement("li")
-    li.innerText = message
-    ul.appendChild(li)
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
 }
 
 function showRoom(){
-    console.log("asddddddd")
-    welcome.hidden = true
-    room.hidden = false
-    const h3 = document.querySelector('h3')
-    h3.innerText = `Room ${roomName}`
+    console.log("asddddddd");
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = document.querySelector('h3');
+    h3.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event){
-    event.preventDefault()
-    const input = document.querySelector("input")
-    socket.emit('enter_room', {payload : input.value}, showRoom)
-    roomName = input.value
-    input.value = ""
+    event.preventDefault();
+    const input = document.querySelector("input");
+    socket.emit('enter_room', {payload : input.value}, showRoom);
+    roomName = input.value;
+    input.value = "";
 }
 
-form.addEventListener("submit", handleRoomSubmit)
+form.addEventListener("submit", handleRoomSubmit);
 
 socket.on("welcome", () => {
-    console.log("A")
-    addMessage("someone joined")
+    console.log("A");
+    addMessage("someone joined");
 })
 
 socket.on("bye", () => {
-    console.log("B")
-    addMessage("someone  left")
+    console.log("B");
+    addMessage("someone  left");
 })
 
--- src/server.js
+++ src/server.js
@@ -3,68 +3,42 @@ import http from "http"
 import SocketIO from "socket.io"
 
 
-const app = express()
+const app = express();
 
-app.set("view engine", "pug")
-app.set("views", __dirname + "/views")
-app.use("/public", express.static(__dirname + "/public"))
-app.get("/", (_, res)=>{res.render("home")})
-app.get("/*", (_, res)=>{res.redirect("/")})
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res)=>{res.render("home")});
+app.get("/*", (_, res)=>{res.redirect("/")});
 
-const handleListen = ()=>console.log(`listening on https://localhost:3000`)
-const httpServer = http.createServer(app)
-const ws = SocketIO(httpServer)
+const handleListen = ()=>console.log(`listening on https://localhost:3000`);
+const httpServer = http.createServer(app);
+const ws = SocketIO(httpServer);
 
 ws.on("connection", (socket) =>{
     socket.onAny((event) => {
-        console.log(`socket event : ${event}`)
+        console.log(`socket event : ${event}`);
     })
 
     socket.on('enter_room', (roomName, showRoom) => {
-        socket.join(roomName)
-        showRoom()
-        socket.to(roomName).emit("welcome")
-        console.log("asdf")
+        console.log(typeof(roomName.payload))
+        socket.join(roomName.payload);
+        showRoom();
+        socket.to(roomName.payload).emit("welcome");
+        
     })
 
     socket.on('disconnecting', () => {
         socket.rooms.forEach((room) => {
-            socket.to(room).emit('bye')
+            socket.to(room).emit('bye');
         });
     })
 })
 
 
-function socketDisconnected(){console.log("client 연결 끊김")}
+function socketDisconnected(){console.log("client 연결 끊김");}
 
-//접속한 socket 저장
-// const webSockets = []
-
-// //webSocket은 브라우저와의 연결
-// wss.on("connection",(webSocket)=>{
-//     webSockets.push(webSocket)
-//     webSocket["nickname"] ="Anon"
-
-//     webSocket.on("close", socketDisconnected)
-//     console.log("connected to browser")
-//     webSocket.on("message", (msg)=>{
-//         const message = JSON.parse(msg)
-        
-//         switch(message.type){
-//             case "new_message":
-//                 console.log(message.payload)
-//                 webSockets.forEach((aSocket) => 
-//                     aSocket.send(`${webSocket.nickname} : ${message.payload}`
-//                 ))
-//             case "nickname":
-//                 console.log(message.payload)
-//                 webSocket["nickname"] = message.payload
-//         }
-        
-//     })
-// }) 
-
-httpServer.listen(3000, handleListen)
+httpServer.listen(3000, handleListen);
 
 
 
